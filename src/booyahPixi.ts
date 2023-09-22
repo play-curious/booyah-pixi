857ad9c3102cb5d2b993a72d2b5ff1e7546e04fd
Jesse Himmelstein <jesse@heavenstone.net>
2023-09-22 15:02:27 +0200

    Fixes to PIXI.Point types

--- src/booyahPixi.ts
+++ src/booyahPixi.ts
@@ -5,13 +5,13 @@ import * as chip from "booyah/src/chip";
 import * as util from "booyah/src/util";
 
 /** Returns the vector length of a a PIXI Point */
-export function magnitude(a: PIXI.Point): number {
+export function magnitude(a: PIXI.IPointData): number {
   return Math.sqrt(a.x * a.x + a.y * a.y);
 }
 
 /** Returns a copy of the PIXI Point x that has a magnitude between min and max */
 export function clampMagnitude(
-  a: PIXI.Point,
+  a: PIXI.IPointData,
   min: number,
   max: number
 ): PIXI.Point {
@@ -21,26 +21,30 @@ export function clampMagnitude(
   } else if (mag > max) {
     return multiply(a, max / mag);
   } else {
-    return a;
+    return new PIXI.Point(a.x, a.y);
   }
 }
 
 /** Returns the distance between two PIXI Points */
-export function distance(a: PIXI.Point, b: PIXI.Point): number {
+export function distance(a: PIXI.IPointData, b: PIXI.IPointData): number {
   const x = a.x - b.x;
   const y = a.y - b.y;
   return Math.sqrt(x * x + y * y);
 }
 
 /** Linear interpolation between points a and b, using the fraction p */
-export function lerpPoint(a: PIXI.Point, b: PIXI.Point, p: number): PIXI.Point {
+export function lerpPoint(
+  a: PIXI.IPointData,
+  b: PIXI.IPointData,
+  p: number
+): PIXI.Point {
   const x = b.x - a.x;
   const y = b.y - a.y;
   return new PIXI.Point(a.x + p * x, a.y + p * y);
 }
 
 /** Returns the sum of PIXI points */
-export function add(...points: PIXI.Point[]): PIXI.Point {
+export function add(...points: PIXI.IPointData[]): PIXI.Point {
   const r = new PIXI.Point();
   for (const p of points) {
     r.x += p.x;
@@ -50,7 +54,7 @@ export function add(...points: PIXI.Point[]): PIXI.Point {
 }
 
 /** Returns the difference of PIXI points */
-export function subtract(...points: PIXI.Point[]): PIXI.Point {
+export function subtract(...points: PIXI.IPointData[]): PIXI.IPointData {
   const r = new PIXI.Point(points[0].x, points[0].y);
   for (let i = 1; i < points.length; i++) {
     r.x -= points[i].x;
@@ -60,27 +64,27 @@ export function subtract(...points: PIXI.Point[]): PIXI.Point {
 }
 
 /** Returns the multiplication of a PIXI point by a scalar */
-export function multiply(a: PIXI.Point, p: number): PIXI.Point {
+export function multiply(a: PIXI.IPointData, p: number): PIXI.Point {
   return new PIXI.Point(a.x * p, a.y * p);
 }
 
 /** Returns the division of a PIXI point by a scalar */
-export function divide(a: PIXI.Point, p: number): PIXI.Point {
+export function divide(a: PIXI.IPointData, p: number): PIXI.Point {
   return new PIXI.Point(a.x / p, a.y / p);
 }
 
 /** Returns a PIXI point with each element rounded down */
-export function floor(p: PIXI.Point): PIXI.Point {
+export function floor(p: PIXI.IPointData): PIXI.Point {
   return new PIXI.Point(Math.floor(p.x), Math.floor(p.y));
 }
 
 /** Returns a PIXI point with each element rounded */
-export function round(p: PIXI.Point): PIXI.Point {
+export function round(p: PIXI.IPointData): PIXI.Point {
   return new PIXI.Point(Math.round(p.x), Math.round(p.y));
 }
 
 /** Returns a PIXI point that has the minimum of each component */
-export function min(...points: PIXI.Point[]): PIXI.Point {
+export function min(...points: PIXI.IPointData[]): PIXI.Point {
   const r = new PIXI.Point(Infinity, Infinity);
   for (const p of points) {
     r.x = Math.min(p.x, r.x);
@@ -90,7 +94,7 @@ export function min(...points: PIXI.Point[]): PIXI.Point {
 }
 
 /** Returns a PIXI point that has the maximum of each component */
-export function max(...points: PIXI.Point[]): PIXI.Point {
+export function max(...points: PIXI.IPointData[]): PIXI.Point {
   const r = new PIXI.Point(-Infinity, -Infinity);
   for (const p of points) {
     r.x = Math.max(p.x, r.x);
@@ -100,12 +104,16 @@ export function max(...points: PIXI.Point[]): PIXI.Point {
 }
 
 /** Returns true if the point p is between points min and max */
-export function inRectangle(p: PIXI.Point, min: PIXI.Point, max: PIXI.Point) {
+export function inRectangle(
+  p: PIXI.IPointData,
+  min: PIXI.IPointData,
+  max: PIXI.IPointData
+) {
   return p.x >= min.x && p.x <= max.x && p.y >= min.y && p.y <= max.y;
 }
 
 /** Takes the mean of PIXI points */
-export function average(...points: PIXI.Point[]): PIXI.Point {
+export function average(...points: PIXI.IPointData[]): PIXI.Point {
   let sum = new PIXI.Point();
   for (const point of points) sum = add(sum, point);
   return divide(sum, points.length);
@@ -116,8 +124,8 @@ export function average(...points: PIXI.Point[]): PIXI.Point {
  Will not "overshoot" b.
  */
 export function moveTowards(
-  a: PIXI.Point,
-  b: PIXI.Point,
+  a: PIXI.IPointData,
+  b: PIXI.IPointData,
   speed: number
 ): PIXI.Point {
   const d = distance(a, b);
@@ -128,8 +136,8 @@ export const moveTowardsPoint = moveTowards;
 
 /** Returns a random point between a amd b, with each component considered separately */
 export function randomPointInRange(
-  min: PIXI.Point,
-  max: PIXI.Point
+  min: PIXI.IPointData,
+  max: PIXI.IPointData
 ): PIXI.Point {
   return new PIXI.Point(
     util.randomInRange(min.x, max.x),
@@ -138,7 +146,7 @@ export function randomPointInRange(
 }
 
 /** Creates a vector pointing in the direction angle, with the length magnitude */
-export function vectorFromAngle(angle: number, magnitude = 1): PIXI.Point {
+export function vectorFromAngle(angle: number, magnitude = 1): PIXI.IPointData {
   return new PIXI.Point(
     Math.cos(angle) * magnitude,
     Math.sin(angle) * magnitude
@@ -147,9 +155,9 @@ export function vectorFromAngle(angle: number, magnitude = 1): PIXI.Point {
 
 /* Returns true if point is within distance d of otherPoints */
 export function withinDistanceOfPoints(
-  point: PIXI.Point,
+  point: PIXI.IPointData,
   d: number,
-  otherPoints: PIXI.Point[]
+  otherPoints: PIXI.IPointData[]
 ): boolean {
   for (const otherPoint of otherPoints) {
     if (distance(point, otherPoint) <= d) return true;
