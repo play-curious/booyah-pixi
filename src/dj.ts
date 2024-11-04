2891f8c7ad88218c4c5924d1cbef06849fd4701e
NICCOLI Samuel (ginahe) <115354631+GinahePro@users.noreply.github.com>
2024-11-04 16:55:14 +0100

    Update dj.ts

--- src/dj.ts
+++ src/dj.ts
@@ -1,12 +1,146 @@
-import * as sound from "@pixi/sound";
-import * as PIXI from "pixi.js";
-
 import * as chip from "booyah/dist/chip";
+import * as PIXI from "pixi.js";
+import * as sound from "@pixi/sound";
+
+class DJChannelOptions {
+  volume: number;
+  singleInstance: boolean;
+  loop: boolean;
+}
+
+type DJChannels = {
+  musique: DJChannelOptions;
+  soundEffect: DJChannelOptions;
+};
+
+class PlayingOptions {
+  volume: number = 1;
+  loop: boolean = false;
+}
+
+class DJChannel {
+  private _playingInstances: Array<sound.IMediaInstance>;
+  private _channelFilter: sound.Filter;
+  private _volumeNode: GainNode;
+
+  constructor(
+    private readonly _getSoundRessource: (
+      name: string,
+    ) => sound.Sound | undefined,
+    private readonly options: DJChannelOptions,
+  ) {
+    this._volumeNode = sound.sound.context.audioContext.createGain();
+    this._volumeNode.gain.value = this.options.volume;
+    this._channelFilter = new sound.filters.Filter(this._volumeNode);
+    this._playingInstances = [];
+  }
+
+  public play(name: string, options?: Partial<PlayingOptions>) {
+    const soundRessource = this._getSoundRessource(name);
+
+    if (this.options.singleInstance) {
+      if (
+        this._playingInstances.length === 1 &&
+        soundRessource.instances.includes(this._playingInstances[0])
+      ) {
+        return;
+      }
+
+      this.stop();
+    }
+
+    const completeOptions = chip.fillInOptions(options, new PlayingOptions());
+
+    //because for some reason play don't last between context
+    soundRessource.filters = [this._channelFilter];
+
+    const instance = sound.sound.play(name, {
+      loop: this.options.loop,
+      volume: completeOptions.volume,
+      complete: () => {
+        const index = this._playingInstances.findIndex((playedInstance) => {
+          return playedInstance.id === instance.id;
+        });
+        this._playingInstances.splice(index);
+      },
+    }) as sound.IMediaInstance;
+
+    this._playingInstances.push(instance);
+
+    return instance.id;
+  }
+
+  public set volume(value: number) {
+    this._volumeNode.gain.value = value;
+  }
+
+  public get volume() {
+    return this._volumeNode.gain.value;
+  }
+
+  public stop() {
+    this._playingInstances.forEach(this._stopInstance);
+  }
+
+  public pause() {
+    this._playingInstances.forEach(this._pauseInstance);
+  }
+
+  public resume() {
+    this._playingInstances.forEach(this._resumeInstance);
+  }
+
+  public stopInstance(id: number) {
+    const instance = this._playingInstances.find((instance) => {
+      return instance.id === id;
+    });
+
+    if (instance) {
+      this._stopInstance(instance);
+    }
+  }
+
+  private _stopInstance(instance: sound.IMediaInstance) {
+    instance.stop();
+  }
+
+  public pauseInstance(id: number) {
+    const instance = this._playingInstances.find((instance) => {
+      return instance.id === id;
+    });
+
+    if (instance) {
+      this._pauseInstance(instance);
+    }
+  }
+
+  private _pauseInstance(instance: sound.IMediaInstance) {
+    instance.set("paused", true);
+  }
+
+  public resumeInstance(id: number) {
+    const instance = this._playingInstances.find((instance) => {
+      return instance.id === id;
+    });
+
+    if (instance) {
+      this._resumeInstance(instance);
+    }
+  }
+
+  private _resumeInstance(instance: sound.IMediaInstance) {
+    instance.set("paused", false);
+  }
+}
 
 export class DJOptions {
   musicChannelVolume = 0.25;
   fxChannelVolume = 1;
   missingAssetBehavior: "exception" | "warning" | "ignore" = "exception";
+}
+
+export class PlayingSoundOptions<AvailableChannel> {
+  channel: AvailableChannel;
 }
 
 export class PlayingMusicOptions {
@@ -37,102 +171,97 @@ export class Dj extends chip.ChipBase {
   private _playingFx: Record<string, PlayingFxOptions>;
   private _lastRequestedMusicName?: string;
 
+  private _channels: Record<string, DJChannel>;
+
   constructor(options?: Partial<DJOptions>) {
     super();
-
     this._options = chip.fillInOptions(options, new DJOptions());
   }
 
   protected _onActivate(): void {
     this._musicChannelVolume = this._options.musicChannelVolume;
     this._fxChannelVolume = this._options.fxChannelVolume;
-
     this._playingFx = {};
+    this._channels = {};
+
+    this._channels["music"] = new DJChannel(this._getSoundResource, {
+      volume: 0.25,
+      singleInstance: true,
+      loop: true,
+    });
+
+    this._channels["soundEffect"] = new DJChannel(this._getSoundResource, {
+      volume: 1,
+      singleInstance: false,
+      loop: false,
+    });
+
+    this._channels["forest"] = new DJChannel(this._getSoundResource, {
+      volume: 0.01,
+      singleInstance: true,
+      loop: true,
+    });
   }
 
   _onTerminate() {
-    this.stopMusic();
-    this.stopAllFx();
+    for (const channel in this._channels) {
+      this._channels[channel].stop();
+    }
   }
 
   protected _onPause(): void {
-    this.pauseMusic();
-    this.pauseAllFx();
+    for (const channel in this._channels) {
+      this._channels[channel].pause();
+    }
   }
 
   protected _onResume(): void {
-    this.resumeMusic();
-    this.resumeAllFx();
+    for (const channel in this._channels) {
+      this._channels[channel].resume();
+    }
   }
 
-  pauseMusic(): void {
-    if (!this._playingMusic) return;
-
-    this._getSoundResource(this._playingMusic.name)?.pause();
+  async playSound(
+    name: string,
+    channel: string,
+    options?: Partial<PlayingOptions>,
+  ) {
+    this._channels[channel].play(name, options);
   }
 
-  resumeMusic(): void {
-    if (!this._playingMusic) return;
+  public changeVolume(channel: string, value: number) {
+    this._channels[channel].volume = value;
+  }
 
-    this._getSoundResource(this._playingMusic.name)?.resume();
+  public getVolume(channel: string) {
+    return this._channels[channel].volume;
   }
 
   get musicChannelVolume(): number {
-    return this._musicChannelVolume;
+    return this._channels["music"].volume;
   }
 
   set musicChannelVolume(value: number) {
-    this._musicChannelVolume = value;
-    if (!this._playingMusic) return;
-
-    const resource = this._getSoundResource(this._playingMusic.name);
-    if (resource) {
-      resource.volume =
-        this._playingMusic.volumeScale * this._musicChannelVolume;
-    }
-
+    this._channels["music"].volume = value;
     this.emit("change:musicChannelVolume", value);
   }
 
   async playMusic(name: string, options?: Partial<PlayingMusicOptions>) {
-    console.log("playMusic() called", name, options, this._playingMusic);
-
-    this._lastRequestedMusicName = name;
-
-    // Wait for the music to load, if not already loaded
-    const resource = await PIXI.Assets.load(name);
-
-    // Some other music must have been requested in the meantime
-    if (this._lastRequestedMusicName !== name) return;
-
-    // Don't play the same thing twice
-    if (this._playingMusic && this._playingMusic.name === name) return;
-
-    this.stopMusic();
-
-    const completeOptions = chip.fillInOptions(
-      options,
-      new PlayingMusicOptions()
-    );
-
-    resource.play({
-      loop: completeOptions.loop,
-      singleInstance: true,
-    });
-    // For some reason the volume in play() seems to be ignored, so set it  here...
-    resource.volume = this._musicChannelVolume * completeOptions.volumeScale;
-
-    this._playingMusic = Object.assign({}, completeOptions, { name });
-    console.log("playMusic() end", name, options, this._playingMusic);
+    //console.log("playMusic() called", name, options, this._playingMusic);
+    this._channels["music"].play(name, options);
   }
 
   stopMusic() {
-    if (!this._playingMusic) return;
+    //console.log("stopMusic() called", this._playingMusic);
+    this._channels["music"].stop();
+  }
 
-    console.log("stopMusic() called", this._playingMusic);
+  pauseMusic(): void {
+    this._channels["music"].pause();
+  }
 
-    this._getSoundResource(this._playingMusic.name)?.stop();
-    delete this._playingMusic;
+  resumeMusic(): void {
+    this._channels["music"].resume();
   }
 
   /** Returns sound duration in ms */
@@ -144,80 +273,39 @@ export class Dj extends chip.ChipBase {
   }
 
   async playFx(name: string, options?: Partial<PlayingFxOptions>) {
-    const resource = this._getSoundResource(name);
-    if (!resource) return;
-
-    const completeOptions = chip.fillInOptions(options, new PlayingFxOptions());
-
-    if (completeOptions.duckMusic) {
-      this.pauseMusic();
-    }
-
-    resource.play({
-      loop: completeOptions.loop,
-      complete: () => {
-        delete this._playingFx[name];
-
-        if (completeOptions.duckMusic) {
-          this.resumeMusic();
-        }
-
-        this.emit("complete", name);
-      },
-    });
-
-    // For some reason the volume given to play() seems to be ignored, so let's set it here
-    resource.volume = this._fxChannelVolume * completeOptions.volumeScale;
-
-    this._playingFx[name] = completeOptions;
+    return this._channels["soundEffect"].play(name, options);
   }
 
-  stopFx(name: string): void {
-    this._getSoundResource(name)?.stop();
-    delete this._playingFx[name];
+  stopFx(id: number): void {
+    this._channels["soundEffect"].stopInstance(id);
   }
 
   stopAllFx(): void {
-    for (const name in this._playingFx) {
-      this.stopFx(name);
-    }
+    this._channels["soundEffect"].stop();
   }
 
-  pauseFx(name: string): void {
-    this._getSoundResource(name)?.pause();
+  pauseFx(id: number): void {
+    this._channels["soundEffect"].pauseInstance(id);
   }
 
   pauseAllFx(): void {
-    for (const name in this._playingFx) {
-      this.pauseFx(name);
-    }
+    this._channels["soundEffect"].pause();
   }
 
-  resumeFx(name: string): void {
-    this._getSoundResource(name)?.resume();
+  resumeFx(id: number): void {
+    this._channels["soundEffect"].resumeInstance(id);
   }
 
   resumeAllFx(): void {
-    for (const name in this._playingFx) {
-      this.resumeFx(name);
-    }
+    this._channels["soundEffect"].resume();
   }
 
   get fxChannelVolume(): number {
-    return this._fxChannelVolume;
+    return this._channels["soundEffect"].volume;
   }
 
   set fxChannelVolume(value: number) {
-    this._fxChannelVolume = value;
-
-    for (const name in this._playingFx) {
-      const resource = this._getSoundResource(name);
-      if (resource) {
-        resource.volume =
-          this._fxChannelVolume * this._playingFx[name].volumeScale;
-      }
-    }
-
+    this._channels["soundEffect"].volume = value;
     this.emit("change:fxChannelVolume", value);
   }
 
@@ -242,7 +330,7 @@ export class PlayMusic extends chip.ChipBase {
 
   constructor(
     private readonly _trackName: string,
-    options?: Partial<PlayingMusicOptions>
+    options?: Partial<PlayingMusicOptions>,
   ) {
     super();
 
@@ -263,7 +351,7 @@ export class PlayFx extends chip.ChipBase {
 
   constructor(
     private readonly _trackName: string,
-    options?: Partial<PlayingFxOptions>
+    options?: Partial<PlayingFxOptions>,
   ) {
     super();
 
