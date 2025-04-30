8e3fdffe6da6f671b365dcbf1d7da9d4b182a4a0
Jesse Himmelstein <jesse@heavenstone.net>
2025-04-30 15:11:24 +0200

    Fixed layout bug with directional layout chips

--- src/layout.ts
+++ src/layout.ts
@@ -1389,6 +1389,7 @@ export class SpriteChip extends DisplayObjectLeafChip<PIXI.Sprite> {
     }
 
     this.updateNaturalInnerSize();
+    this.requestResize();
   }
 }
 
@@ -1926,8 +1927,8 @@ export class DirectionalContainerChip extends ContainerBase<DirectionalContainer
       }
     }
 
-    const innerLocalBounds = this._childResizeInfo!.localBounds;
-    const innerAbsoluteBounds = this._childResizeInfo!.absoluteBounds;
+    const innerLocalBounds = this._lastResizeInfo!.localBounds;
+    const innerAbsoluteBounds = this._lastResizeInfo!.absoluteBounds;
 
     // Do a second pass to bring elements to their ideal lengths
     let availableExtraSpace = innerLocalBounds[lengthProp]! - minUsedSpace;
